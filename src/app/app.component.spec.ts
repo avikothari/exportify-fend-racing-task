90bb964abea1bc8ff76f988eb27a47846e436aa0
Avinash Kothari <avinash.kothari@exportify.in>
2022-02-03 13:30:12 +0530

    first commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'trexis-racing'`, () => {
+  it(`should have as title 'exportify-racing'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('trexis-racing');
+    expect(app.title).toEqual('exportify-racing');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('trexis-racing app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('exportify-racing app is running!');
   });
 });
